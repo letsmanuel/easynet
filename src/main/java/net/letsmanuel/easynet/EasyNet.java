0918a19db8ce336f62b643c529d3bee52219daaa
LetsManuel <161684857+letsmanuel@users.noreply.github.com>
2025-06-22 23:40:57 +0200

    Update EasyNet.java

--- src/main/java/net/letsmanuel/easynet/EasyNet.java
+++ src/main/java/net/letsmanuel/easynet/EasyNet.java
@@ -15,10 +15,7 @@ public class EasyNet implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Easy world!");
 	}
 }
